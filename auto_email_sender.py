ed93bd351ac12ee23ab130cf83fa7a8f107a2027
semiromest <semiromest_battle@hotmail.com>
2021-10-24 00:59:02 +0300

    1.1
    Hatalar giderildi.
    Şifre gizliliği sağlandı.

--- auto_email_sender.py
+++ auto_email_sender.py
@@ -1,44 +1,41 @@
-# encoding:utf-8
+# -*- encoding: utf-8 -*-
 import smtplib
 import socket
+from getpass import getpass
 
 mail = smtplib.SMTP("smtp.gmail.com",587)
-
 mail.ehlo()
-
 mail.starttls()
 
-print("1 - Tek Kullanıcıya Mesaj Gönder" + "\n" + "2 - Toplu Mesaj Gönder")
-secenek = int(input("Lütfen yapmak istediğiniz işlemi seçiniz: "))
+print("[+] 1 - Tek Kullanıcıya Mesaj Gönder" + "\n" + "[+] 2 - Toplu Mesaj Gönder")
+secenek = int(input("[+] Lütfen yapmak istediğiniz işlemi seçiniz: "))
+
 if secenek == 1:
 
-     content = input(str("Lütfen Göndermek İstediğiniz Mesajı Giriniz:"))
-     print(content)
-
-     emailadresi = input(str("Lütfen Email Adresinizi Giriniz:"))
-
-     sifre = int(input("Lütfen Şifrenizi Giriniz:"))
-
-     mail_kimden = input(str("Lütfen Hangi Email Adresinden Mail Göndereceğinizi Giriniz:"))
-
-     mail_kime = input(str("Lütfen Hangi Email Adresine Mail Göndereceğinizi Giriniz:"))
+     content = input("[+] Lütfen Göndermek İstediğiniz Mesajı Giriniz:"))
+     emailadresi = input("[+] Lütfen Email Adresinizi Giriniz:"))
+     sifre = getpass("[+] Lütfen Şifrenizi Giriniz:"))
+     mail_kimden = input("[+] Lütfen Hangi Email Adresinden Mail Göndereceğinizi Giriniz:"))
+     mail_kime = input("[+] Lütfen Hangi Email Adresine Mail Göndereceğinizi Giriniz:"))
 
      mail.login(emailadresi,sifre)
-
      mail.sendmail(mail_kimden , mail_kime,content)
+     
 if secenek == 2:
-    emailadres_list = input(str("Lütfen Email Adresi Listesinin Dizinini Giriniz:"))
 
+    emailadres_list = input("[+] Lütfen Email Adreslerinin Bulunduğu Text Uzantılı Dosyanın Dizinini Giriniz:"))
     emailadres_list = open(emailadres_list,"r")
-
-    content = input(str("Lütfen Göndermek İstediğiniz Mesajı Giriniz:"))
-
-    mail_kimden = input(str("Lütfen Hangi Email Adresinden Mail Göndereceğinizi Giriniz:"))
+    content = input("[+] Lütfen Göndermek İstediğiniz Mesajı Giriniz:"))
+    mail_kimden = input("[+] Lütfen Hangi Email Adresinden Mail Göndereceğinizi Giriniz:"))
 
     for email in emailadres_list:
       try:
           mail.sendmail(mail_kimden,emailadres_list)
-          print("[+] Emailler gönderildi!")
+          print("[+] Mailler gönderildi!")
           break
       except smtplib.SMTPConnectError :
-          print("Bağlantı Hatası")
+          print("[+] Bağlantı Hatası")
+    emailadres_list.close()
+else :
+   print("[+] Yanlış seçim yaptınız!")
+
